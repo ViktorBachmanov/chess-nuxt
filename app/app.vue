01cacf16e5fa7d768756cfac4bfcd84c35a0d49d
Viktor Bachmanov <vbachmanov@mail.ru>
2025-08-17 19:01:20 +0300

    create main.css file

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,22 @@
+<script setup>
+async function handleLogin() {
+  await $fetch('/api/login', {
+    method: "POST",
+    body: {
+      username: 'ustas',
+      password: 'pass123'
+    }
+  })
+}
+</script>
+
 <template>
-  <div>
-    Chess-Nuxt
-  </div>
+  <UApp>
+    Chess-Nuxt <ColorModeButton /><br>
+    <UButton
+      @click="handleLogin"
+    >
+      Log in
+    </UButton>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,26 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@sidebase/nuxt-auth']
+  modules: ['@nuxt/ui', '@sidebase/nuxt-auth'],
+  css: ['~/assets/css/main.css'],
+  auth: {
+    provider: {
+      type: 'local',
+      endpoints: {
+        signIn: { path: '/login', method: 'post' },
+        signOut: { path: '/logout', method: 'post' },
+      },
+      token: {
+        signInResponseTokenPointer: '/token',
+        type: 'Bearer',
+        cookieName: 'auth.token',
+        headerName: 'Authorization',
+        maxAgeInSeconds: 1800,
+        sameSiteAttribute: 'lax',
+        cookieDomain: 'sidebase.io',
+        secureCookieAttribute: false,
+        httpOnlyCookieAttribute: false,
+      }
+    }
+  }
 })
