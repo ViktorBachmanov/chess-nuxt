d75f930dbf5783f5f9842744116f1e6a4ee10f25
Viktor Bachmanov <vbachmanov@mail.ru>
2025-08-19 09:31:43 +0300

    read request headers and body

--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -1,5 +1,13 @@
 export default defineEventHandler(async (event) => {
-  // const body = await readBody(event)
+  const allHeaders = event.node.req.headers
+  // console.log('All headers:', allHeaders)
+  const userAgent = allHeaders['user-agent']
+  console.log('userAgent:', userAgent)
+
+  const formData = await readBody(event)
+  console.log('formData:', formData)
+
+
   return { 
     token: 'AUTH_TOKEN',
    }
