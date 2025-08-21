e6e65eda51ec1fa7aee46886134a1a9366ad140d
Viktor Bachmanov <vbachmanov@mail.ru>
2025-08-21 09:23:51 +0300

    as well show users without games

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
   const users = db.query(
-    `SELECT u.id, u.name, u.rating, u.score, COUNT(*) AS games FROM users u 
-     JOIN games g ON (u.id = g.white OR u.id = g.black)
+    `SELECT u.id, u.name, u.rating, u.score, COUNT(g.id) AS games FROM users u 
+     LEFT JOIN games g ON (u.id = g.white OR u.id = g.black)
      GROUP BY u.id
      ORDER BY rating DESC`
   )
