b39ae67a19fadee8e7803f9e0255ba703722935a
Viktor Bachmanov <vbachmanov@mail.ru>
2025-08-25 09:03:14 +0300

    correct style

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -29,14 +29,14 @@ function handleGamesUpdated() {
 
 <template>
   <div
-    class="flex justify-between p-4 shadow-lg dark:bg-slate-800"
+    class="flex items-center justify-between p-4 shadow-lg dark:bg-slate-800"
   >
     <AdminMenu
       @game-added="handleGamesUpdated"
       @game-deleted="handleGamesUpdated"
     />
 
-    <h1 class="font-medium text-2xl">
+    <h1 class="font-medium text-xl sm:text-2xl">
       Шахматный клуб
     </h1>
 
